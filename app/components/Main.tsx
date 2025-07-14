93585ad1aa4493df528992362dac072ba25a4acf
Yarr <ii@qstudio.org>
2025-07-14 16:12:49 +0300

    feature(add): Font Lato

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -4,17 +4,18 @@ import CardIcon from '@/app/public/icon_card.svg';
 
 export default function Main() {
   return (
-    <div className="bg-red-300 max-w-md w-full">
-      <h1>main</h1>
-      <div>
+    <div className="bg-red-300 max-w-md w-full px-5 py-8">
+      <div className="flex flex-row justify-between items-center">
         <BurgermenuIcon />
-        <div>
-          <div>Luminae</div>
+
+        <div className="flex flex-row">
+          <div className="font-bold text-[22px]">Luminae</div>
           <LogoIcon />
         </div>
-        <div>
+
+        <div className="flex flex-row">
           <CardIcon />
-          <div className="rounded-[50%] w-6 h-6 bg-[#3DC47E] grid place-items-center">3</div>
+          <div className="rounded-[50%] w-6 h-6 bg-mint-green grid place-items-center">3</div>
         </div>
       </div>
     </div>
